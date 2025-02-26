95a0836b190d9f8f554149ac1f96a781edab4f32
Ignacio Zaldo González <nachozaldo3@gmail.com>
2025-02-26 20:57:04 +0100

    subida_inicial

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  // img = 'assets/pelotaPaint.png';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
